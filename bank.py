d6c51165813ca0a55d436bd08aef361de025e1c9
shullmb <shullmb@gmail.com>
2018-07-25 14:28:55 -0700

    lab complete

--- bank.py
+++ bank.py
@@ -1,11 +1,62 @@
 class BankAccount:
-  pass
 
-class ChildrensAccount:
-  pass
+  def __init__(self):
+    self.balance = 0
+    self.interest_rate = 0.02
+  
+  def deposit(self, amount):
+    # add money and return balance
+    # return False if amount negative
+    if amount < 0:
+      return False
+    else:
+      self.balance += amount
+      return self.balance
+  
+  def withdraw(self, amount):
+    # subtract money and return balance
+    # return False if neg amount or if withdrawal is > amount
+    if amount > self.balance or amount < 0:
+      return False
+    else: 
+      self.balance -= amount
+      return self.balance
+    
+  def accumulate_interest(self):
+    # return balance * 1.02
+    self.balance *= (1 + self.interest_rate)
+    return self.balance
 
-class OverdraftAccount:
-  pass
+class ChildrensAccount(BankAccount):
+  def __init__(self):
+    super().__init__()
+    self.interest_rate = 0
+
+  def accumulate_interest(self):
+    # add $10 when called
+    self.balance += 10
+    return self.balance  
+
+class OverdraftAccount(BankAccount):
+  def __init__(self):
+    super().__init__()
+    self.overdraft_penalty = 40
+  
+  def withdraw(self, amount):
+    if amount < 0:
+      return False
+    elif amount > self.balance:
+      self.balance -= self.overdraft_penalty
+      return self.balance
+    else:
+      self.balance -= amount
+      return self.balance
+
+  def accumulate_interest(self):
+    if self.balance < 0:
+      return self.balance
+    else:
+      self.balance *= (1 + self.interest_rate)
 
 basic_account = BankAccount()
 basic_account.deposit(600)
